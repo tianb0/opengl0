019b001172a4322fdcb416414983826ed509da11
Tianbo <tianbo@hey.com>
2023-04-22 19:14:50 -0700

    vertex data -> graphics card memory

---
+++ src/triangle/triangle.cpp
@@ -0,0 +1,85 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main() {
+  // init glfw and configure
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for macos
+#endif
+
+  // create glfw window
+  GLFWwindow *window =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "hello", NULL, NULL);
+  if (window == NULL) {
+    std::cerr << "failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // glad: load all opengl function pointers
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  // set up vertex data
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f, // left
+    0.5f, -0.5f, 0.0f, // right
+    0.0f, 0.5f, 0.0f // top
+  };
+
+  // -- 1
+  // send vertex data as the first process of graphics pipeline: the vertext shader
+  unsigned int VBO; // vertex buffer objects
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind vertex buffer
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // copies vertex data into buffer
+  // -- 1, vertex data is stored on graphics card memory and managed by VBO buffer
+
+
+  // render loop
+  while (!glfwWindowShouldClose(window)) {
+    // input
+    processInput(window);
+
+    // rendering commands here
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // state setting
+    glClear(GL_COLOR_BUFFER_BIT);         // state using
+
+    // check and call events and swap the buffers
+    glfwSwapBuffers(window); // double buffers
+    glfwPollEvents(); // poll IO events (keys pressed/released, mouse move, etc)
+  }
+
+  glfwTerminate(); // clean up
+  return 0;
+}
+
+// process all input: query glfw wheter relevant keys are pressed or released in
+// this frame and process
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+// glfw: a callbacl to be called when window size changed, by OS or user.
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
